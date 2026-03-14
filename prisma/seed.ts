c04436cf240d1db5ec80a8e7d927e23491d29c01
aggrm <alberto.goujon@gmail.com>
2026-03-14 18:20:55 +0100

    Creacion de base de datos con Prisma y Neon, incluyendo modelos, migraciones y seed data.

---
+++ prisma/seed.ts
@@ -0,0 +1,42 @@
+import { categories } from "./data/categories"
+import { products } from "./data/products"
+
+import { PrismaClient } from "@/src/generated/prisma/client"
+import { PrismaNeon } from "@prisma/adapter-neon"
+
+import { neonConfig } from "@neondatabase/serverless"
+import ws from "ws"
+
+neonConfig.webSocketConstructor = ws
+
+const prisma = new PrismaClient({
+  adapter: new PrismaNeon({
+    connectionString: process.env.DATABASE_URL!
+  })
+})
+
+async function main() {
+  try {
+
+    await prisma.category.createMany({
+      data: categories
+    })
+
+    await prisma.product.createMany({
+      data: products
+    })
+    console.log('Seed ejecutado correctamente')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
