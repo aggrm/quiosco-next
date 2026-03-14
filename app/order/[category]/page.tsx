7ec8ae2a4b291d33cf723f3ae8b709c6de3e4df8
aggrm <alberto.goujon@gmail.com>
2026-03-14 19:21:47 +0100

    Añadida las imagenes de las categorias y productos. Ademas de hacer dinamico nuestro enrutamiento de categorias.

---
+++ app/order/[category]/page.tsx
@@ -0,0 +1,6 @@
+
+export default function OrderPage(params : { category: string  }) {
+  return (
+    <div>OrderPage</div>
+  )
+}
---
+++ components/ui/CategoryIcon.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image"
+import { Category } from "@/src/generated/prisma/client"
+import Link from "next/link"
+
+type CategoryIconProps = {
+    category: Category
+}
+
+export default function CategoryIcon({category} :  CategoryIconProps) {
+    return (
+        <div className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
+            <div className="relative size-16">
+                <Image 
+                    src={`/icon_${category.slug}.svg`}
+                    alt={`Imagen de la categoria ${category.name}`}
+                    fill
+                />
+            </div>
+            <Link 
+                className="text-lg font-bold"
+                href={`/order/${category.slug}`}
+            >{category.name}</Link>
+        </div>
+    )
+}
